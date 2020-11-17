a9df264d76d9066fd5474528842cd4f747d2674e
Veronique <veroniqueauzias@gmail.com>
2020-11-17 19:02:00 +0100

    add quest 03-Symfony

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/programs/", name="program_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
