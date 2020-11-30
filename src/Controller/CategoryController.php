cdc1d2cb85b0a246b78d7de67b31a459374bb136
Veronique <veroniqueauzias@gmail.com>
2020-11-30 17:02:44 +0100

    Q8 - list all programs in categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,8 +4,10 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -29,5 +31,28 @@ class CategoryController extends AbstractController
             ['categories' => $categories]
         );
     }
+    /**
+     * Getting a category by name
+     *
+     *@Route("/{categoryName}", name="show")
+     *@return Response
+     */
 
+    public function show(string $categoryName): Response
+    {
+        $category = $this->getDoctrine()
+        ->getRepository(Category::class)
+        ->findBy(['name' => $categoryName]);
+
+        if(!$category)
+            throw new NotFoundHttpException('Category not found');
+
+        $programs = $this ->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(['category'=> $category], ['id' => 'desc'], 3);
+
+        return $this->render('category/show.html.twig', [
+            'programs' => $programs
+        ]);
+    }
 }
