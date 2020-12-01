2ec2425d44f942211d24987c3f5fd42499cd3541
Veronique <veroniqueauzias@gmail.com>
2020-12-01 17:38:18 +0100

    add attributes and methods in Entities Category and Program according to quest course

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -33,7 +33,7 @@ class Program
     private $poster;
 
     /**
-     * @ORM\ManyToOne(targetEntity=Category::class)
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="programs")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
